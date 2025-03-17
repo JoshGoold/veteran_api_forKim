5c28e9e5761b65dca23e18644657d9b920ad94bf
JoshGoold <joshgoold2@hotmail.com>
2025-03-17 14:06:23 -0400

    ...

--- routes/validateEmail.js
+++ routes/validateEmail.js
@@ -3,10 +3,11 @@ const Selection = require("../models/SelectionSchema")
 
 const app = express.Router()
 
-app.post("/validate", async (req,res)=> {
+app.post("/validate-email", async (req,res)=> {
     const {email} = req.body;
     try {
         const user = await Selection.findOne({email: email});
+        console.log(user)
         if(!user) return res.status(404).send({Message: "That email has not been used", Success: false})
         return res.send({Message: "Validated", Success: true})
     } catch (error) {
